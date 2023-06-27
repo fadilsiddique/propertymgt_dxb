e81803ed4eea907c21468ab3b69a3c84424978e2
fadilsid <fadilsiddique@gmail.com>
2023-06-27 22:52:57 +0000

    feat: add rooms in flats when room is created

--- propertymgt_dxb/property_management/doctype/room/room.py
+++ propertymgt_dxb/property_management/doctype/room/room.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2023, HyperCloud and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Room(Document):
-	pass
+	def after_insert(self):
+		flat = frappe.get_doc('Flat',self.flat_no)
+
+		flat.append("rooms",{
+			"room_no":self.name
+		})
+		flat.save()
