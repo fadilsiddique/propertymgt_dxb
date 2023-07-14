e677c53d94a90dae8556a22dcd51e5f030536e93
fadilsid <fadilsiddique@gmail.com>
2023-07-14 00:36:46 +0000

    fix: minor fix

--- propertymgt_dxb/property_management/doctype/room_bill/room_bill.py
+++ propertymgt_dxb/property_management/doctype/room_bill/room_bill.py
@@ -16,15 +16,13 @@ class RoomBill(Document):
 		pdf_data = frappe.get_print(self.doctype,self.name, print_format = 'Room Bill', as_pdf=True)
 		# pdf_data_base64 = base64.b64encode(pdf_data).decode('utf-8')
 		# pdf_file_path = get_files_path(f"Room Bill/{self.name}.pdf", is_private=0)
-		save_file(f"{self.name}.pdf",pdf_data,self.doctype,self.name,"Home/Attachments/Room Bill",is_private=0)
+		file_id=save_file(f"{self.name}.pdf",pdf_data,self.doctype,self.name,"Home/Attachments/Room Bill",is_private=0)
 
-		# document_url="4e8b1baa13"
-		document_path = f"/files/{self.name}.pdf"
-		# direct_url = get_file_url(document_url)
-		direct_url = document_path
-		print(direct_url)
+
+		# document_path = f"/files/{self.name}.pdf"
+		direct_url = get_file_url(file_id.name)
 		message = f"Attached the flat bill of {self.flat_no}"
 
-		frappe.enqueue(sendMessage,queue="default", message=message,document_path=direct_url)
+		frappe.enqueue(sendMessage,queue="short", message=message,document_path=direct_url)
 
 		# sendMessage(message=message,document_path=direct_url)
